b712000badcaa42dcdc7bb8bba900570269db0f3
Bezalel3639 <bezalel3639@gmail.com>
2019-06-12 11:15:14 +0300

    Packaged to bundle.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,85 +1,81 @@
 import React, { Component } from 'react';
 
 class Portfolio extends Component {
-  constructor(props) {
-    super();
-      this.state = {
-        symbol: 'BTC',
-        newsymbol: '',
-        rows: [
-          {
-            symbol: 'ETH',
-            amount: 3.44
-          },
-          {
+    constructor(props) {
+        super();
+        this.state = {
             symbol: 'BTC',
-            amount: 0.35
-          }
-    ]}
-  }
+            newsymbol: '',
+            rows: [
+                {
+                    symbol: 'ETH',
+                    amount: 3.44
+                },
+                {
+                    symbol: 'BTC',
+                    amount: 0.35
+                }
+            ]
+        }      
+    }
+  
+    addRow () {
+        const item = {symbol: this.state.newsymbol, amount: ""};
+        this.setState({rows: [...this.state.rows, item]}); // "..." expands
+    }
 
-  addRow = () => {
-    console.log (this.state.newsymbol);
-    const item = {symbol: this.state.newsymbol, amount: ""};
+    updateAdd (e) {
+        this.setState({ newsymbol: e.target.value });
+    }
+  
+    updateRow (index, e) {
+        const { name, value } = e.target;
+        const rows = [...this.state.rows];
+        const symbol = rows[index].symbol;
+        rows[index] = {symbol: symbol, amount: value};
+        this.setState({ rows });
+    };
 
-    this.setState({rows: [...this.state.rows, item]}); // "..." expands
-  }
+    deleteRow (index) {
+        const rows = [...this.state.rows]
+        rows.splice(index, 1)
+        this.setState({ rows })
+    }
 
-  updateAdd = (e) => {
-    this.setState({ newsymbol: e.target.value });
-  }
-
-  updateRow = index => e => {
-      const { name, value } = e.target;
-      const rows = [...this.state.rows];
-
-      const symbol = rows[index].symbol;
-      rows[index] = {symbol: symbol, amount: value};
-
-      this.setState({ rows });
-  };
-
-  deleteRow = (index) => () => {
-    const rows = [...this.state.rows]
-    rows.splice(index, 1)
-    this.setState({ rows })
-  }
-
-  render() {
-    return (
-
-    <div>
-    <h3 align="center">Investor Portfolio: {this.props.investor}</h3>
-    <div align="center">
-      <input type="text" size="5" onChange={this.updateAdd}/>
-      &nbsp;&nbsp;
-      <button onClick={this.addRow}>Add</button>
-    </div>
-    &nbsp;
-    <div align="center">
-            <table border="1">
-              <thead>
-                <tr><th>#</th><th>&nbsp;Symbol&nbsp;</th><th>Amount</th><th>&nbsp;</th></tr>
-              </thead>
-              <tbody>
-              {this.state.rows.map((item, index) => (
-                   <tr key={index}>
-                     <td>{index}</td>
-                     <td>{this.state.rows[index].symbol}</td>
-                     <td><input
-                          type="text"
-                          value={this.state.rows[index].amount}
-                          onChange={this.updateRow(index)}
-                        /></td>
-                     <td><button onClick={this.deleteRow(index)}>Delete</button></td>
-                   </tr>
-                 ))}
-             </tbody>
-            </table>
-    </div>
-    </div>
-    );
-  }
-}
+    render() {
+        return (
+            <div align="center">
+                <h3 align="center">Investor Portfolio: {this.props.investor}</h3>
+                <div>
+                    <input type="text" size="5" onChange={this.updateAdd.bind(this)}/>
+                    &nbsp;&nbsp;
+                    <button onClick={this.addRow.bind(this)}>Add</button>
+                </div>
+                &nbsp;
+                <table border="1">
+                    <thead>
+                        <tr><th>#</th><th>&nbsp;Symbol&nbsp;</th><th>Amount</th><th>&nbsp;</th></tr>
+                    </thead>
+                    <tbody>
+                    {
+                        this.state.rows.map((item, index) => (
+                            <tr key={index}>
+                                <td>{index}</td>
+                                <td>{this.state.rows[index].symbol}</td>
+                                <td><input
+                                    type="text"
+                                    value={this.state.rows[index].amount}
+                                    onChange={this.updateRow.bind(this, index)}
+                                /></td>
+                                <td><button onClick={this.deleteRow.bind(this, index)}>Delete</button></td>
+                            </tr>
+                        ))
+                    }
+                    </tbody>
+                </table>
+            </div>      
+        );
+    }
+}  
 
 export default Portfolio;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var BUILD_DIR = path.resolve(__dirname, 'src');
+var APP_DIR = path.resolve(__dirname, 'dist');
+
+module.exports = {
+   
+    entry: './src/App.js',
+
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+ 
+    module : {
+        rules : [    
+            {
+                test : /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader : "babel-loader"
+                }
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+};
