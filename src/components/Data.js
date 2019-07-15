6942a2441414035db8bb5238dab8175aa45391cf
Bezalel3639 <bezalel3639@gmail.com>
2019-07-15 13:53:53 +0300

    New file for sandboxes mockup data

---
+++ src/components/Data.js
@@ -0,0 +1,9 @@
+// Waves
+export const adress_data = [{
+        address: "3N12JNDNAYNE2dRSGbxK3UQXfVJnXaMwiYT",
+        seed: "XXXXX lazy lens fix dwarf salad breeze myself silly december endless rent faculty report beyond"
+    }, {
+        address: "one more address",
+        seed: "one more seed"
+    }
+]
