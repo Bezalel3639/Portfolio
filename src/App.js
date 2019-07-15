02efb0bce9dced52c5f1527b78501ce41c04107d
Bezalel3639 <bezalel3639@gmail.com>
2019-07-15 13:28:27 +0300

    Bootstrap support added, ngrok address is set in parent component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Portfolio from './components/Portfolio'
+import {render} from 'react-dom';
+import Portfolio from './components/Portfolio';
+import BalanceSandbox from './components/Balance_Sandbox';
+import ReservesSandbox from './components/Reserves_Sandbox';
+import FeeStructureSandbox from './components/FeeStructure_Sandbox';
+import Exchange from './components/Exchange';
+import {Router, Route, Link} from "react-router";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class App extends Component {
     constructor(){
         super();
         this.state = {
             investor: 'Pele'
-        }
+        }       
     }
   
     render() {
-        return (
+        var address = "f27366b7"; // set ngrok address here
+        
+        return (            
             <div className="App">
-                <Portfolio investor={this.state.investor} />
+                <Exchange />
+                &nbsp;    
+                <Portfolio investor={this.state.investor} ngrok_address={address} />
+                &nbsp;
+                <BalanceSandbox investor={this.state.investor} ngrok_address={address} />
+                &nbsp;
+                <ReservesSandbox ngrok_address={address} /> 
+                &nbsp;
+                <FeeStructureSandbox />                         
             </div>
         );
-    }
+    }  
  }
 
 ReactDOM.render(<App />, document.getElementById('portfolio'));
