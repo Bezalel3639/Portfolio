b2cc6e72cc6d6d7aa556e9d256c3966fcc447411
Bezalel3639 <bezalel3639@gmail.com>
2020-08-13 11:51:11 +0300

    A RUB withdrawal from multi-user wallet to PayPal is OK (test #102)

--- src/components/Balance_Sandbox.js
+++ src/components/Balance_Sandbox.js
@@ -1,88 +1,129 @@
 import React, { Component } from 'react';
 import ModalDialogDeposit from './ModalDialog_Deposit';
+import ModalDialogDepositFromPayPal from './ModalDialog_DepositFromPayPal';
 import ModalDialogWithdraw from './ModalDialog_Withdraw';
+import ModalDialogWithdrawFromFintechGateway from './ModalDialog_WithdrawFromFintechGateway';
 import { Button } from 'react-bootstrap';
 import $ from 'jquery';
-import {endpoints_base} from './Data';
+import { endpoints_base } from './Data';
 
 class BalanceSandbox extends Component {
     constructor(props) {
         super();
         this.state = {
-            modalShow: false,
+            modalDepositShow: false,
+            modalDepositFromPayPalShow: false,
             modalWithdrawShow: false,
-            symbol: 'BTC',
-            newsymbol: '',
-            rows: [
-                {
-                    symbol: 'ETH',
-                    address: '0x8d8057d0810996077effe2283ef5788178a91e61',
-                    explorer: 'https://ropsten.etherscan.io/address/',                    
-                    balance: 'N/A'
-                },
-                {
-                    symbol: 'BTC',
-                    address: '2N6V12gcR2XaFGFMGK7qeNTjqcxo3wAzWdp',
-                    explorer: 'https://live.blockcypher.com/btc-testnet/address/',
-                    balance: 0.0
-                },
-                {
-                    symbol: 'BTC',
-                    address: 'mowu56fBGxQtB7CAkS3b39mDy56YGMSfTR',
-                    explorer: 'https://live.blockcypher.com/btc-testnet/address/',
-                    balance: 0.0
-                }, 
-                {
-                    symbol: 'LTC',
-                    address: 'N/A',
-                    explorer: 'N/A',
-                    balance: 'N/A'
-                },  
-                {
-                    symbol: 'WAVES',
-                    address: '3MqnoW5aY4x2eiwmM4ee1VjWwuJEvKnffj8',
-                    explorer: 'https://wavesexplorer.com/testnet/address/',
-                    balance: 0.0
-                },
-                {
-                    symbol: 'USD',
-                    address: 'PayPal',
-                    explorer: 'N/A',
-                    balance: 'N/A'
-                }
-           ]
-        }      
+            modalWithdrawFromFintechGatewayShow: false,
+            selectedData: {}, 
+            rows: [],
+            currentRow: '',
+            currentSymbol: ''       
+        }
+        
+        this.callback_balanceUpdate = this.callback_balanceUpdate.bind(this);
     }
 
     componentWillMount(){
-        // ETH
-        this.getURLData(0, endpoints_base + "/v1/Ethereum/Testnet/Balance/0x8d8057d0810996077effe2283ef5788178a91e61");
-        
-        // BTC 
-        this.getURLData(1, endpoints_base + "/v1/Bitcoin/Testnet/Balance/2N6V12gcR2XaFGFMGK7qeNTjqcxo3wAzWdp");
-     
-        // BTC 
-        this.getURLData(2, endpoints_base + "/v1/Bitcoin/Testnet/Balance/mowu56fBGxQtB7CAkS3b39mDy56YGMSfTR");
-
-        // Waves rate
-        this.getURLData(4, endpoints_base + "/v1/Waves/Testnet/Balance/3MqnoW5aY4x2eiwmM4ee1VjWwuJEvKnffj8"); 
+        this.getMongoDBData(endpoints_base + "/v1/BackOffice/GetSandboxBalancesAssets/" + this.props.investor);     
     }
 
-    getURLData(index, URL){
+    callback_balanceUpdate(data) {
+        console.log("callback_balanceUpdate> " + data);
+        var balance = parseFloat(data.replace(",", "."));
+        console.log("callback_balanceUpdate> balance: " + balance);
+        const rows = [...this.state.rows];  
+        console.log("callback_balanceUpdate> rows: " + rows);
+        console.log("callback_balanceUpdate> currentRow: " + this.state.currentRow);
+        const current_symbol = rows[this.state.currentRow].name;
+        const current_address = rows[this.state.currentRow].address;
+        const current_explorer = rows[this.state.currentRow].explorer;
+        rows[this.state.currentRow] = {name: current_symbol, address: current_address, explorer: current_explorer, amount: balance} ; // 45.55};
+        this.setState({ rows });
+    }
+    
+    getMongoDBData(URL) {
         $.ajax({
             url: URL, 
             dataType: 'json',
             contentType: 'json',
             cache: false,
-            success: function(data){       
-                const rows = [...this.state.rows];
-                const current_symbol = rows[index].symbol;
+            success: function(data) {
+                console.log("getMongoDBData> :", data);
+                this.populateTable(data); 
+            }.bind(this),
+                error: function(xhr, status, err){           
+                console.log(err);
+            }
+        });
+    }
+
+    populateTable(data) {
+        var data_update = new Array();
+
+        // Modify array of objects to include amount, and block explorer.        
+        var element1 = {amount: 'N/A'};
+        for (let i = 0; i < data.length; i++) {
+            var obj = data[i];
+            var element2 = {explorer: this.getCryptoAssetExplorer(data[i].name)};
+            const returnedTarget = Object.assign(obj, element1, element2);
+            data_update[i] = returnedTarget;
+        }
+
+        this.setState({ rows: data_update });
+        console.log("populateTable> :", this.state.rows);       
+
+        // Update amounts from endpoints
+        for (let i = 0; i < data.length; i++) {
+            this.updateAmount(i, data[i].name, data[i].address);
+        }
+    }
+
+    updateAmount(index, asset, address) {
+        if (asset == 'BTC') {
+            URL = endpoints_base + "/v1/Bitcoin/Testnet/Balance/" + address; 
+            this.getURLBalanceData(index, URL);
+        } else if (asset == 'ETH') {
+            URL = endpoints_base + "/v1/Ethereum/Testnet/Balance/" + address;
+            this.getURLBalanceData(index, URL);
+        } else if (asset == 'WAVES') {
+            URL = endpoints_base + "/v1/Waves/Testnet/Balance/" + address;
+            this.getURLBalanceData(index, URL); 
+        } else if (asset == 'RUB') {
+            URL = endpoints_base + "/v1/PayPal/sandbox/GetCustomerBalance/" + this.props.investor + "/" + "RUB";
+            this.getURLBalanceData(index, URL); 
+            console.log ("updateAmount> Currency: " + asset + ", address: " + address);
+        }  else if (asset == 'USD') {
+            URL = endpoints_base + "/v1/PayPal/sandbox/GetCustomerBalance/" + this.props.investor + "/" + "USD";
+            this.getURLBalanceData(index, URL); 
+            console.log ("updateAmount> Currency: " + asset + ", address: " + address);
+        }  else if (asset == 'HKD') {
+           URL = endpoints_base + "/v1/PayPal/sandbox/GetCustomerBalance/" + this.props.investor + "/" + "HKD";
+           this.getURLBalanceData(index, URL); 
+           console.log ("updateAmount> Currency: " + asset + ", address: " + address);
+        } else {
+            console.log ("Currency is fiat");
+        }
+    }
+
+    getURLBalanceData(index, URL){
+        console.log("getURLBalanceData> index:", index); 
+        console.log("getURLBalanceData> URL:", URL); 
+        $.ajax({
+            url: URL, 
+            dataType: 'json',
+            contentType: 'json',
+            cache: false,
+            success: function(data){ 
+                console.log("getURLBalanceData> :", data);         
+                const rows = [...this.state.rows];  
+                const current_symbol = rows[index].name;
                 const current_address = rows[index].address;
                 const current_explorer = rows[index].explorer;
-                const current_balance = rows[index].balance;
-                rows[index] = {symbol: current_symbol, address: current_address, explorer: current_explorer, balance: data};
+                rows[index] = {name: current_symbol, address: current_address, explorer: current_explorer, amount: data};
                 this.setState({ rows });
 
+                // TODO: do I need it?
                 // Pass rows to parent
                 const {dataCallback_userBalanceSandbox} = this.props; 
                 dataCallback_userBalanceSandbox(rows);
@@ -92,80 +133,112 @@ class BalanceSandbox extends Component {
             }
         });
     }
+
+    getCryptoAssetExplorer(asset) {
+        if (asset == 'BTC') {
+            return "https://live.blockcypher.com/btc/address/"; 
+        } else if (asset == 'LTC') {
+            return "https://live.blockcypher.com/ltc/address/";
+        } else if (asset == 'ETH') {
+            return "https://etherscan.io/address/";
+        } else if (asset == 'WAVES') {
+            return "https://wavesblockexplorer.com/address/";
+        } else {
+            return "Cryptosymbol is not found";
+        }         
+    } 
+    
+    isShowLink(asset) {
+        if (asset == 'BTC' || asset == 'ETH' || asset == 'WAVES') {
+            return true; 
+        } else {
+            return false;
+        }
+        return false;
+    }
+
+    isCrypto(asset) {
+        if (asset == 'BTC' || asset == 'LTC' || asset == 'ETH' || asset == 'WAVES') {
+            return true; 
+        } else {
+            return false;
+        }
+        return false;
+    }
+
+    getUnits(asset) {
+        console.log("getUnits>", asset)
+        var units = new Object();
+        if (asset == 'BTC') {
+            units.name = "Satoshi";
+            units.base = 100000000;
+        } else if (asset == 'LTC') {
+            units.name = "Microlitecoins"; // "Photons"
+            units.base = 1000000; 
+        } else if (asset == 'ETH') {
+            units.name = "Wei";
+            units.base = 1000000000000000000; // 1 ETH = 1 000 000 000 000 000 000 WEI (18 zeros)
+        } else if (asset == 'WAVES') {
+            units.name = "Waves base units";
+            units.base = 100000000; 
+        } else {
+            return null;
+        }
+        return units;
+    }
+
+    handlerButtonWithdrawClick(object) {
+        this.setState({ selectedData: object })
+        this.setState({ modalWithdrawShow: true })        
+    }
   
     render() { 
-        let modalClose = () => this.setState({ modalShow: false });  
-        let modalWithdrawClose = () => this.setState({ modalWithdrawShow: false });       
+        let modalDepositClose = () => this.setState({ modalDepositShow: false });  
+        let modalDepositFromPayPalClose = () => this.setState({ modalDepositFromPayPalShow: false }); 
+        let modalWithdrawClose = () => this.setState({ modalWithdrawShow: false });
+        let modalWithdrawFromFintechGatewayClose = () => this.setState({ modalWithdrawFromFintechGatewayShow: false });
         return (            
             <div align="center">                
-                <h3 align="center">Investor Sandbox Balance: {this.props.investor}</h3>
+                <h3 align="center">Sandbox Balance: {this.props.investor != '' ? this.props.investor : "Investor is NOT DEFINED"}</h3>
                 <table border="1">
                     <thead>
-                        <tr align="center"><th>#</th><th>&nbsp;Symbol&nbsp;</th><th>&nbsp;Address&nbsp;</th><th>Balance</th><th>&nbsp;</th><th>&nbsp;</th><th>&nbsp;</th></tr>
+                        <tr align="center"><th className='hidden'>#</th><th>&nbsp;Symbol&nbsp;</th><th>&nbsp;Address&nbsp;</th><th>Balance</th><th>&nbsp;</th><th>&nbsp;</th></tr>
                     </thead>
                     <tbody>
                     {
-                        this.state.rows.map((item, index) => (
+                        this.state.rows.map((item, index) => (                            
                             <tr key={index}>
-                                <td>{index}</td> 
-                                <td>{this.state.rows[index].symbol}</td>                               
+                                { /* Row number column */ }
+                                <td className='hidden'>{index}</td>                                 
+                                { /* Asset column */ }
+                                <td>{this.state.rows[index].name}</td>
+                                { /* Address column */ }
                                 {
-                                    this.state.rows[index].symbol == 'ETH'  &&
-                                    <td><a href={this.state.rows[index].explorer + this.state.rows[index].address}>{this.state.rows[index].address}</a></td>
+                                    this.isShowLink(this.state.rows[index].name) ? (
+                                        <td><a href={this.state.rows[index].explorer + this.state.rows[index].address}>{this.state.rows[index].address}</a></td>
+                                    ) : (
+                                        <td>{this.state.rows[index].address}</td> 
+                                    )
                                 }
+                                { /* Balance column */ }
                                 {
-                                    this.state.rows[index].symbol == 'BTC' &&
-                                    <td><a href={this.state.rows[index].explorer + this.state.rows[index].address}>{this.state.rows[index].address}</a></td>
-                                }
-                                {
-                                    this.state.rows[index].symbol == 'LTC'  &&
-                                    <td>{this.state.rows[index].address}</td>
-                                } 
-                                {
-                                    this.state.rows[index].symbol == 'WAVES' &&
-                                    <td><a href={this.state.rows[index].explorer + this.state.rows[index].address}>{this.state.rows[index].address}</a></td>
-                                }
-                                {
-                                    this.state.rows[index].symbol == 'USD'  &&
-                                    <td>{this.state.rows[index].address}</td>
-                                } 
-
-                                {
-                                    this.state.rows[index].symbol == 'ETH' &&
-                                    /* 1 ETH = 1 000 000 000 000 000 000 WEI (18 zeros) */                              
-                                    <td>{this.state.rows[index].balance / 1000000000000000000} ({this.state.rows[index].balance} <b>wei</b>)</td>
-                                } 
-                                {
-                                    this.state.rows[index].symbol == 'BTC' &&
-                                    <td>{this.state.rows[index].balance / 100000000} ({this.state.rows[index].balance} <b>satoshi</b>)</td>
-                                } 
-                                {
-                                    this.state.rows[index].symbol == 'LTC' &&
-                                    <td>{this.state.rows[index].balance}</td>
-                                }
-                                {
-                                    this.state.rows[index].symbol == 'WAVES' &&
-                                    <td>{this.state.rows[index].balance / 100000000} ({this.state.rows[index].balance} <b>units</b>)</td>
-                                }
-                                {
-                                    this.state.rows[index].symbol == 'USD' &&
-                                    <td>{this.state.rows[index].balance}</td>
-                                } 
-                                {
-                                    this.state.rows[index].symbol == 'HKD' &&
-                                    <td>{this.state.rows[index].balance}</td>
-                                }
- 
-                                <td><Button onClick={()=>this.setState({ modalShow: true })}>Deposit</Button></td>
-                                <td><Button onClick={()=>this.setState({ modalWithdrawShow: true })}>Withdraw</Button></td>
-                                <td><Button>Delete</Button></td>
+                                    this.isCrypto(this.state.rows[index].name) ? (
+                                        <td title={this.state.rows[index].amount + " (" + this.getUnits(this.state.rows[index].name).name + ")"}>{Number(this.state.rows[index].amount / this.getUnits(this.state.rows[index].name).base).toFixed(5)}</td>
+                                        ) : (
+                                        <td>{this.state.rows[index].amount != 'N/A' ? this.state.rows[index].amount.toFixed(2) : 'N/A'}</td>
+                                    ) 
+                                }                             
+                                <td><Button onClick={()=>{ this.isCrypto (this.state.rows[index].name) ? this.setState({ modalDepositShow: true }) : this.setState({ currentRow: index, modalDepositFromPayPalShow: true })} }>Deposit</Button></td>
+                                <td><Button onClick={()=>{ this.isCrypto (this.state.rows[index].name) ? this.setState({ modalWithdrawShow: true }) : this.setState({ currentRow: index, currentSymbol: this.state.rows[index].name, modalWithdrawFromFintechGatewayShow: true })} }>Withdraw</Button></td>
                             </tr>
                         ))
                     }
                     </tbody>                    
                 </table>
-                <ModalDialogDeposit show={this.state.modalShow} onHide={modalClose}/>
-                <ModalDialogWithdraw show={this.state.modalWithdrawShow} onHide={modalWithdrawClose}/> 
+                <ModalDialogDeposit show={this.state.modalDepositShow} onHide={modalDepositClose}/>
+                <ModalDialogDepositFromPayPal user={this.props.investor} index={this.state.currentRow} callback_balanceupdate = {this.callback_balanceupdate} show={this.state.modalDepositFromPayPalShow} onHide={modalDepositFromPayPalClose}/>     
+                <ModalDialogWithdraw data={this.state.selectedData} show={this.state.modalWithdrawShow} onHide={modalWithdrawClose}/> 
+                <ModalDialogWithdrawFromFintechGateway callback_balanceUpdate = {this.callback_balanceUpdate} user={this.props.investor} symbol={this.state.currentSymbol} data={this.state.selectedData} show={this.state.modalWithdrawFromFintechGatewayShow} onHide={modalWithdrawFromFintechGatewayClose}/> 
             </div>      
         );
     }
